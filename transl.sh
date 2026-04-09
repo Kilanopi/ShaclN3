da1175be636a3ec5667fa62d7b9bab5fa94a1257
Kilanopi <kilanopi@gmail.com>
2026-04-09 16:14:53 +0200

    revert to pre turtle and fix node and prop hierarchy

--- transl.sh
+++ transl.sh
@@ -8,14 +8,14 @@ curr=$( pwd)
 if [[ $1 ]] && [ -f $1 ]; then
     echo "Working with $1"
     echo "--------------------------------------------------------"
-    eye --turtle $1 --pass --nope --quiet --no-qvars > grounded.n3
+    eye $1 --pass --nope --quiet --no-qvars > grounded.n3
     echo "grounded version of $1 created under grounded.n3:"
     echo "--------------------------------------------------------"
     cat grounded.n3
     echo "--------------------------------------------------------"
     echo "creating translation of $1 into N3"
     echo "..."
-    eye $SCRIPT_DIR/proc/targeting.n3 $SCRIPT_DIR/core/CoreConstraintsNode.n3 $SCRIPT_DIR/core/CoreConstraintsProp.n3 $SCRIPT_DIR/core/NotConstraint.n3 --turtle grounded.n3 --pass-only-new --nope --quiet > translated.n3
+    eye $SCRIPT_DIR/proc/targeting.n3 $SCRIPT_DIR/core/CoreConstraintsNode.n3 $SCRIPT_DIR/core/CoreConstraintsProp.n3 $SCRIPT_DIR/core/NotConstraint.n3 grounded.n3 --pass-only-new --nope --quiet > translated.n3
     echo "translated version of $1 created under translated.n3:"
     echo "--------------------------------------------------------"
     cat translated.n3
